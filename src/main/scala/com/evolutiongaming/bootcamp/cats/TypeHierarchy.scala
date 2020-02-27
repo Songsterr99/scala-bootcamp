cd284f01b98c4a9c1dedc19e8b37c097da5da7df
Daniil_Naumau <daniil_naumau@epam.com>
2020-02-27 17:34:24 +0300

    hierarchy is done

--- src/main/scala/com/evolutiongaming/bootcamp/cats/TypeHierarchy.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/cats/TypeHierarchy.scala
@@ -47,31 +47,42 @@ object TypeHierarchy {
 }
 
 object Excercises {
+
   trait Applicative[F[_]] extends Functor[F] {
-    def map[A,B](fa: F[A])(f: A => B): F[B]
+    def map[A, B](fa: F[A])(f: A => B): F[B]
 
     def unit[A](a: => A): F[A]
 
-    // implement methods using other methods
-    def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+    def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = unit(map(fab)(f => map(fa)(a => f(a))))
 
-    def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = ???
+    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = apply(map(fa)(a => (b: B) => (a, b)))(fb)
 
-    def sequence[A](fas: List[F[A]]): F[List[A]] = ???
+    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = map(product(fa, fb))(a => f(a._1, a._2))
 
-    def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = ???
+
+    def sequence[A](fas: List[F[A]]): F[List[A]] = fas.foldRight(unit(List.empty[A])) {(a, result) =>
+      map2(a, result)(_ :: _)
+    }
+
+    def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] = as.foldRight(unit(List.empty[B])) { (a, result) =>
+      map2(f(a), result)(_ :: _)
+    }
   }
 
   trait Monad[M[_]] extends Functor[M] {
     def unit[A](a: => A): M[A]
 
-    // implement methods using other methods
-    def flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = ???
+    def apply[A, B](mab: M[A => B])(ma: M[A]): M[B] = unit(map(mab)(f => map(ma)(a => f(a))))
 
-    def join[A](mma: M[M[A]]): M[A] = ???
+    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] = join(map(ma)(f))
 
-    def map[A,B](ma: M[A])(f: A => B): M[B] = ???
+    def product[A, B](fa: M[A], fb: M[B]): M[(A, B)] = apply(map(fa)(a => (b: B) => (a, b)))(fb)
 
-    def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] = ???
+    def join[A](mma: M[M[A]]): M[A] = flatMap(mma)(ma => ma)
+
+    def map[A, B](ma: M[A])(f: A => B): M[B] = apply(unit(f))(ma)
+
+    def map2[A, B, C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] = map(product(ma, mb))(a => f(a._1, a._2))
   }
+
 }
